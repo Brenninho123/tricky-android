9a012351cabbb14c175a5ead845bbbfe3b23665a
justaguy6 <outerjose@gmail.com>
2024-09-27 15:06:41 -0300

    bruh

--- source/mobile/flixel/FlxVirtualPad.hx
+++ source/mobile/flixel/FlxVirtualPad.hx
@@ -201,7 +201,7 @@ class FlxVirtualPad extends FlxSpriteGroup
 		button.immovable = true;
 		button.scrollFactor.set();
 		button.color = Color;
-		button.alpha = ClientPrefs.padalpha;
+//		button.alpha = ClientPrefs.padalpha;
 		#if FLX_DEBUG
 		button.ignoreDrawDebug = true;
 		#end
